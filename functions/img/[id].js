b040ce878155dbf48a525391f512fabe3d129c00
bionhard-dev <bionhard@gmail.com>
2026-05-22 16:33:33 -0400

    Update [id].js

--- functions/img/[id].js
+++ functions/img/[id].js
@@ -1,1 +1,11 @@
-
+export async function onRequest(context) {
+  return new Response(
+    "Proxy activo correctamente",
+    {
+      status: 200,
+      headers: {
+        "Content-Type":"text/plain"
+      }
+    }
+  );
+}
